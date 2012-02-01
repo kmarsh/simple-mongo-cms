9b000c977280e3e190f2e7cfc9a1fa2fe45e6f0b
Kevin Marsh <kevin.marsh@gmail.com>
2012-02-01 17:05:08 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,101 @@
+require 'rubygems'
+require 'yaml'
+require 'liquid'
+require 'sinatra'
+require 'mongo'
+
+class RecordDrop < Liquid::Drop
+  def initialize(record)
+    @record = record
+  end
+
+  def before_method(method)
+    @record[method]
+  end
+end
+
+class CollectionDrop < Liquid::Drop
+  def before_method(method)
+    $db[method].find().map {|r| RecordDrop.new(r) }
+  end
+end
+
+module Liquid
+  class MongoFileSystem < Liquid::BlankFileSystem
+    def read_template_file(template_path, context)
+      snippet = $db['snippets'].find(:name => template_path).first
+      return nil if snippet.nil?
+
+      snippet['body']
+    end
+  end
+end
+
+Liquid::Template.file_system = Liquid::MongoFileSystem.new
+
+header = <<LIQUID
+<h1>&lt;form&gt; &amp;&amp; f(x)</h1>
+LIQUID
+
+home = <<LIQUID
+{% include 'header' %}
+<ul>
+  {% for page in db.pages %}
+  <li><a href="{{ page.path }}">{{ page.title }}</a></li>
+  {% endfor %}
+</ul>
+
+<p>Welcome! Blah blah</p>
+LIQUID
+
+team = <<LIQUID
+{% include 'header' %}
+<h2>Team</h2>
+<ul>
+{% for person in db.team %}
+  <li>{{ person.name }}</li>
+{% endfor %}
+</ul>
+LIQUID
+
+recent_work = <<LIQUID
+{% include 'header' %}
+<h2>Recent Work</h2>
+<ul>
+{% for person in db.team %}
+  <li>{{ person.name }}</li>
+{% endfor %}
+</ul>
+LIQUID
+
+$mongo = Mongo::Connection.new
+$db = $mongo['ffx-cms']
+
+$db['snippets'].remove
+$db['snippets'].insert('name' => 'header', 'body' => header)
+
+$db['pages'].remove
+$db['pages'].insert('path' => '/', 'title' => 'Home', 'body' => home)
+$db['pages'].insert('path' => '/team', 'title' => 'Team', 'body' => team)
+$db['pages'].insert('path' => '/recent-work', 'title' => 'Recent Work', 'body' => recent_work)
+
+$db['team'].remove
+%w[Paul Kevin Scott].each do |person|
+  $db['team'].insert('name' => person)
+end
+
+get '*' do
+  route = params[:splat].join('/')
+
+  template = $db['pages'].find(:path => route).first
+
+  halt 404 if template.nil?
+
+  @template = Liquid::Template.parse(template['body'])
+
+  assigns = {
+    'db' => CollectionDrop.new
+  }
+
+  @template.render(assigns)
+end
