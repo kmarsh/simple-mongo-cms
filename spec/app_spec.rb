6592bda7e1beb785f76855784230f9b52930293c
Kevin Marsh <kevin.marsh@gmail.com>
2012-02-10 18:30:24 -0500

    Fix up specs

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,34 +1,34 @@
-# encoding: UTF-8
 require File.dirname(__FILE__) + '/spec_helper'
-
 
 describe 'Main' do
   
   include Rack::Test::Methods
   
-  describe "simple page" do
-    subject 
+  before do
+    $mongo = Mongo::Connection.new
+    $db = $mongo['ffx-cms']
+
+    $db['snippets'].remove
+    $db['snippets'].insert('name' => 'header', 'body' => "header")
+
+    $db['pages'].remove
+    $db['pages'].insert('path' => '/', 'title' => 'Home', 'body' => "home")
+    $db['pages'].insert('path' => '/team', 'title' => 'Team', 'body' => "team")
   end
 
-  it "should be homepage" do
+  it "should GET /" do
     get '/'
     last_response.should be_ok
   end
 
-  it "should be ok for 'new'" do
+  it "should GET" do
     get '/new'
     last_response.should be_ok
   end
 
-
   describe "missing pages" do
-    it "should be 404 for 'no-page-by-this-name'" do
+    it "should be 404 for a page that doesn't exist" do
       get '/no-page-by-this-name'
-      last_response.status.should == 404
-    end
-
-    it "should be missing for '/no-page-by-this-name/edit'" do
-      get '/about/no-page-by-this-name'
       last_response.status.should == 404
     end
   end
